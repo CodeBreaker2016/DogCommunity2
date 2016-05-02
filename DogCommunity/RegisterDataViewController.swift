cfe9dc917359965eebe4f09b5b4bffd196b8e1f3
Alumno <alumno@imac-de-itesm-10.local>
2016-05-02 15:58:29 -0500

    changes to views and added register .swift file

---
+++ DogCommunity/RegisterDataViewController.swift
@@ -0,0 +1,37 @@
+//
+//  RegisterDataViewController.swift
+//  DogCommunity
+//
+//  Created by Alumno on 02/05/16.
+//  Copyright © 2016 TEAM PUE. All rights reserved.
+//
+
+import Parse
+
+import UIKit
+
+class RegisterDataViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
